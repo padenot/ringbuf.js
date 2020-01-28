2073965b7ac0c79f636fe6049a9ae568e46926ec
Spencer Rudnick <spencer@synth.kitchen>
2020-01-28 22:35:50 +0100

    Rollup config

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+import pkg from './package.json'
+
+export default {
+    input: 'js/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        resolve(),
+        commonjs()
+    ]
+}

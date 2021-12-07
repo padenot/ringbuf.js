02027704b44a18a8dca7d49e81dbbbb16e5b2ec5
Paul Adenot <paul@paul.cx>
2021-12-07 15:42:00 +0100

    Initial lint fixes for audioworklet-to-worker

--- public/example/audioworklet-to-worker/recorder-worklet.js
+++ public/example/audioworklet-to-worker/recorder-worklet.js
@@ -1,19 +1,19 @@
-var exports = {};
+const exports = {};
 
 class RecorderWorklet extends AudioWorkletProcessor {
   constructor(options) {
     super();
     // Staging buffer to interleave the audio data.
     this.interleaved = new Float32Array(128 * 2); // stereo
-    var sab = options.processorOptions;
+    const sab = options.processorOptions;
     this._audio_writer = new AudioWriter(new RingBuffer(sab, Float32Array));
   }
 
-  process(inputs, outputs, parameters) {
+  process(inputs, _outputs, _parameters) {
     // interleave and store in the queue
     if (inputs[0]) {
       interleave(inputs[0], this.interleaved);
-      if (this._audio_writer.enqueue(this.interleaved) != 256) {
+      if (this._audio_writer.enqueue(this.interleaved) !== 256) {
         console.log("underrun: the worker doesn't dequeue fast enough!");
       }
     }
--- public/example/audioworklet-to-worker/wav-writer.js
+++ public/example/audioworklet-to-worker/wav-writer.js
@@ -1,24 +1,26 @@
-let exports = {};
+const exports = {};
 
 // Read some float32 pcm from the queue, convert to int16 pcm, and push it to
 // our global queue.
 function readFromQueue() {
-  var samples_read = this._audio_reader.dequeue(this.staging);
+  const samples_read = this._audio_reader.dequeue(this.staging);
   if (!samples_read) {
     return 0;
   }
-  var segment = new Int16Array(samples_read);
-  for (var i = 0; i < samples_read; i++) {
+  const segment = new Int16Array(samples_read);
+  for (let i = 0; i < samples_read; i++) {
     segment[i] = Math.min(Math.max(this.staging[i], -1.0), 1.0) * (2 << 14);
   }
   pcm.push(segment);
+  return samples_read;
 }
 
-onmessage = function(e) {
-  switch(e.data.command) {
-    case "init" : {
-      this._audio_reader =
-        new exports.AudioReader(new RingBuffer(e.data.sab, Float32Array));
+onmessage = function (e) {
+  switch (e.data.command) {
+    case "init": {
+      this._audio_reader = new exports.AudioReader(
+        new RingBuffer(e.data.sab, Float32Array)
+      );
       // The number of channels of the audio stream read from the queue.
       this.channelCount = e.data.channelCount;
       // The sample-rate of the audio stream read from the queue.
@@ -33,43 +35,48 @@ onmessage = function(e) {
       // staging buffer size = ring buffer size / sizeof(float32) / stereo / 4
       this.staging = new Float32Array(e.data.sab.byteLength / 4 / 4 / 2);
       // Attempt to dequeue every 100ms. Making this deadline isn't critical:
-      // there's 1 second worth of space in the queue, and we'll be dequeing 
+      // there's 1 second worth of space in the queue, and we'll be dequeing
       interval = setInterval(readFromQueue, 100);
       break;
     }
-    case "stop" : {
+    case "stop": {
       clearInterval(interval);
       // Drain the ring buffer
-      while (readFromQueue()) { }
+      while (readFromQueue()) {
+        /* empty */
+      }
       // Structure of a wav file, with a byte offset for the values to modify:
       // sample-rate, channel count, block align.
       const CHANNEL_OFFSET = 22;
       const SAMPLE_RATE_OFFSET = 24;
       const BLOCK_ALIGN_OFFSET = 32;
-      var header = [
+      const header = [
         // RIFF header
         0x52, 0x49, 0x46, 0x46, 0x00, 0x00, 0x00, 0x00, 0x57, 0x41, 0x56, 0x45,
         // fmt chunk. We always write 16-bit samples.
-        0x66, 0x6d, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0xFF, 0xFF,
-        0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x10, 0x00,
+        0x66, 0x6d, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x10, 0x00,
         // data chunk
-        0x64, 0x61, 0x74, 0x61, 0xFE, 0xFF, 0xFF, 0x7F];
+        0x64, 0x61, 0x74, 0x61, 0xfe, 0xff, 0xff, 0x7f,
+      ];
       // Find final size: size of the header + number of samples * channel count
       // * 2 because pcm16
-      var size = header.length;
-      for (var i = 0; i < this.pcm.length; i++) {
+      let size = header.length;
+      for (let i = 0; i < this.pcm.length; i++) {
         size += this.pcm[i].length * 2;
       }
-      let wav = new Uint8Array(size);
-      let view = new DataView(wav.buffer);
+      const wav = new Uint8Array(size);
+      const view = new DataView(wav.buffer);
 
-      // Copy the header, and modify the values: note that RIFF 
+      // Copy the header, and modify the values: note that RIFF
       // is little-endian, we need to pass `true` as the last param.
-      for (var i = 0 ; i < wav.length; i++) {
+      for (let i = 0; i < wav.length; i++) {
         wav[i] = header[i];
       }
 
-      console.log(`Writing wav file: ${this.sampleRate}Hz, ${this.channelCount} channels, int16`);
+      console.log(
+        `Writing wav file: ${this.sampleRate}Hz, ${this.channelCount} channels, int16`
+      );
 
       view.setUint16(CHANNEL_OFFSET, this.channelCount, true);
       view.setUint32(SAMPLE_RATE_OFFSET, this.sampleRate, true);
@@ -77,14 +84,18 @@ onmessage = function(e) {
 
       // Finally, copy each segment in order as int16, and transfer the array
       // back to the main thread for download.
-      var writeIndex = header.length;
-      for (var segment = 0; segment < this.pcm.length; segment++) {
-        for (var sample = 0; sample < this.pcm[segment].length; sample++) {
+      let writeIndex = header.length;
+      for (let segment = 0; segment < this.pcm.length; segment++) {
+        for (let sample = 0; sample < this.pcm[segment].length; sample++) {
           view.setInt16(writeIndex, this.pcm[segment][sample], true);
-          writeIndex+=2;
+          writeIndex += 2;
         }
       }
       postMessage(wav.buffer, [wav.buffer]);
+      break;
+    }
+    default: {
+      throw Error("Case not handled");
     }
   }
-}
+};
